b7eb281195f5cb295d23d1360c3c22024e78c434
manu <manuchangole@gmail.com>
2021-01-17 18:19:34 +0300

    Retrofit @path and @query

--- app/src/main/java/echangole/com/retrofitsample/MainActivity.java
+++ app/src/main/java/echangole/com/retrofitsample/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -15,6 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
   TextView textView;
+   private JsonPlaceHolder jsonPlaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +30,61 @@ public class MainActivity extends AppCompatActivity {
                 baseUrl("https://jsonplaceholder.typicode.com")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        JsonPlaceHolder jsonPlaceHolder=retrofit.create(JsonPlaceHolder.class);
+       jsonPlaceHolder=retrofit.create(JsonPlaceHolder.class);
 
-        Call<List<Post>> call=jsonPlaceHolder.getPosts();
+       //getPosts();
+        getComment();
+
+
+
+
+
+    }
+
+    private void getComment()
+    {
+        Call<List<Comment>> call=jsonPlaceHolder.getComments("posts/3/comments");
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response)
+            {
+                if(!response.isSuccessful())
+                {
+                    textView.setText("Code"+response.code());
+                    return;
+                }
+
+                List<Comment> comments=response.body();
+                for(Comment comment:comments)
+                {
+                    String content="";
+                    content+="Id:"+comment.getId()+"\n";
+                    content+="PostId:"+comment.getPostId()+"\n";
+                    content+="name:"+comment.getName()+"\n";
+                    content+="email:"+comment.getEmail()+"\n";
+                    content+="Body:"+comment.getText()+"\n"+"\n";
+                    textView.append(content);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t)
+            {
+                textView.setText(t.getCause().getMessage());
+
+            }
+        });
+
+    }
+
+    private void getPosts()
+    {
+        Map<String ,String> parameters=new HashMap<>();
+        parameters.put("userId","1");
+        parameters.put("_sort","id");
+        parameters.put("order","desc");
+        Call<List<Post>> call=jsonPlaceHolder.getPosts(parameters);
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response)
@@ -61,7 +116,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
     }
 }
